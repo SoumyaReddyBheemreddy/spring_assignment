e5d88d9d2ff5466f5bc416077ffefad142fb3669
SoumyaReddyBheemreddy <soumyareddy.bheemreddy@zemosolabs.com>
2021-12-30 17:43:44 +0530

    updated updatePetTest

--- src/test/java/com/example/springboot/pet_clinic/controller/PetControllerTest.java
+++ src/test/java/com/example/springboot/pet_clinic/controller/PetControllerTest.java
@@ -64,6 +64,12 @@ class PetControllerTest {
     void savePetTest() throws Exception{
         Pet pet = new Pet(0,"Tom","Cat");
         mockMvc.perform(post("/pets/save").flashAttr("pet",pet)).andExpect(view().name("redirect:/pets/list"));
+        if(pet.getId()!=0){
+            Pet tempPet = petService.findById(pet.getId());
+            tempPet.setName(pet.getName());
+            tempPet.setType(pet.getType());
+            verify(petService,times(1)).save(tempPet);
+        }
         verify(petService,times(1)).save(pet);
     }
     @Test
