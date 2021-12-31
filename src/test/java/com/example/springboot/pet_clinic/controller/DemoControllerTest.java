aa0b0dee1c1fb67b4fb3e4baba1106255d89227b
SoumyaReddyBheemreddy <soumyareddy.bheemreddy@zemosolabs.com>
2021-12-31 12:38:06 +0530

    updated demo controller test

--- src/test/java/com/example/springboot/pet_clinic/controller/DemoControllerTest.java
+++ src/test/java/com/example/springboot/pet_clinic/controller/DemoControllerTest.java
@@ -1,7 +1,6 @@
 package com.example.springboot.pet_clinic.controller;
 
 import com.example.springboot.pet_clinic.config.TestDataSourceConfig;
-import com.example.springboot.pet_clinic.service.PetService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -11,18 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest(classes = TestDataSourceConfig.class)
 @TestPropertySource(locations = "classpath:applicationTest.properties")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class DemoController {
+class DemoControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -32,6 +31,7 @@ class DemoController {
     }
     @Test
     void homeTest() throws Exception {
-        mockMvc.perform(get("/home")).andReturn();
+        mockMvc.perform(get("/home"))
+                .andExpect(MockMvcResultMatchers.view().name("home/welcome"));
     }
 }
