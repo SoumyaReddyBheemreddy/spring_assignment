7944478dca98d843a95e1bae0bac374fab8b4526
SoumyaReddyBheemreddy <soumyareddy.bheemreddy@zemosolabs.com>
2022-01-04 12:38:45 +0530

    updated pet dto

--- src/main/java/com/example/springboot/pet_clinic/converter/PetConverter.java
+++ src/main/java/com/example/springboot/pet_clinic/converter/PetConverter.java
@@ -2,22 +2,31 @@ package com.example.springboot.pet_clinic.converter;
 
 import com.example.springboot.pet_clinic.dto.PetDTO;
 import com.example.springboot.pet_clinic.entity.Pet;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 @Component
 public class PetConverter {
     public PetDTO entityToDto(Pet pet)
     {
-        ModelMapper mapper=new ModelMapper();
-        PetDTO map=mapper.map(pet,PetDTO.class);
-        return map;
+        PetDTO petDTO = new PetDTO();
+        petDTO.setName(pet.getName());
+        petDTO.setId(pet.getId());
+        petDTO.setAppointments(pet.getAppointments());
+        petDTO.setType(pet.getType());
+        petDTO.setOwners(pet.getOwners());
+        return petDTO;
     }
     public List<PetDTO> entityToDto(List<Pet> pets)
     {
-        return  pets.stream().map(pet->entityToDto(pet)).collect(Collectors.toList());
+        List<PetDTO> petsDTO = new ArrayList<>();
+        for(Pet pet: pets){
+            PetDTO petDTO= entityToDto(pet);
+            petsDTO.add(petDTO);
+        }
+        return  petsDTO;
 
     }
 }
